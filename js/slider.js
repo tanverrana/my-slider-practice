4142658af4251523792c5d9079987b8d8bde7b76
tanverrana <tanversobur@gmail.com>
2022-03-12 01:07:55 +0600

    show images

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,16 @@
+const images = [
+    "images/img-1.jpeg",
+    "images/img-2.jpeg",
+    "images/img-3.jpeg",
+    "images/img-4.jpeg",
+    "images/img-5.jpeg"
+];
+let imageIndex = 0;
 setInterval(() => {
-
+    if (imageIndex >= images.length) {
+        imageIndex = 0;
+    }
+    const imgUrl = images[imageIndex];
+    console.log(imgUrl)
+    imageIndex++;
 }, 1000)
